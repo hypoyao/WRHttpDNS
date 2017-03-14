d52752e26f7e687c3a6051321d60897d8650923c
hypoyao <hypoyao@qq.com>
2017-03-14 11:22:19 +0800

    Initial Commit

---
+++ WRHttpDNS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WRHttpDNS
+//
+//  Created by hypo on 2017/3/14.
+//  Copyright © 2017年 hypo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
